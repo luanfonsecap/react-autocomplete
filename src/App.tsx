a354fbc2f2cdd3ba7e661ba5a7624df54ceeb2ee
Luan Fonseca <luanfonsecap@icloud.com>
2020-06-13 13:43:21 -0300

    import autocomplete and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useMemo } from "react";
+
 import "./App.css";
+import Autocomplete from "./components/Autocomplete";
 
 function App() {
-  return <div></div>;
+  const options = useMemo(() => {
+    return [
+      "Azul",
+      "Amarelo",
+      "Vermelho",
+      "Verde",
+      "Laranja",
+      "Preto",
+      "Cinza",
+      "Branco",
+    ];
+  }, []);
+
+  return (
+    <div>
+      <h1>React Autocomplete</h1>
+
+      <Autocomplete suggestions={options} />
+    </div>
+  );
 }
 
 export default App;
